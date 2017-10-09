2da3c7fc4d1431d28e6632ac2c4c2adeb6c2fd6f
mkiken <mkiken0@gmail.com>
2017-10-10 02:01:27 +0900

    redisつなぎこみ

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const REDIS_RANKING_KEY = 'ranking';
+
+var redis = require("redis"),
+    client = redis.createClient();
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
 
 // urlencodedとjsonは別々に初期化する
 app.use(bodyParser.urlencoded({
@@ -14,17 +22,68 @@ app.listen(3000, function () {
   console.log('listening start on port 3000.');
 });
 
-// ランキング取得 API
+/**
+ * ランキング取得 API
+ *
+ * @param req.query.user_id {string} - ユーザーID
+ */
 app.get('/ranking', function (req, res) {
-  res.send('ranking page.');
-});
+  res.header("Content-Type", "application/json; charset=utf-8");
+  if (req.query.user_id == null) {
+    let msg = 'user_id is empty.';
+    res.status(500).json(msg);
+    console.error(ms);
+    return;
+  }
 
-// スコア記録 API
+  client.zrevrank(
+    [
+      REDIS_RANKING_KEY,
+      req.query.user_id
+    ],
+    function (err, response) {
+      if (err) {
+        res.status(500).json(err.message);
+        console.error(err);
+        return;
+      }
+      if (typeof response === 'number'){
+        // 0-originを1-originにする
+        response += 1;
+      }
+      res.json(response);
+    });
+  });
+
+/**
+ * スコア記録 API
+ *
+ * @param req.body {Object} - {user_id: (int), score: (int)}
+ */
 app.post('/score', function(req, res) {
-    // リクエストボディを出力
-    console.log(req.body);
-    // パラメータ名、nameを出力
-    console.log(req.body.name);
+  res.header("Content-Type", "application/json; charset=utf-8");
+  const params = req.body;
+  if (params.user_id == null || params.score == null) {
+    let msg = 'user_id, score is empty.: ' + JSON.stringify(params);
+    res.status(500).json(msg);
+    console.error(msg);
+    return;
+  }
 
-    res.send(req.body);
+  client.zadd(
+    [
+      REDIS_RANKING_KEY,
+      params.score,
+      params.user_id
+    ],
+    function (err, response) {
+      if (err) {
+        res.status(500).json(err.message);
+        console.error(res);
+        return;
+      }
+      console.log('[score] added '+response+' items.');
+
+      res.json(response);
+    });
 });
